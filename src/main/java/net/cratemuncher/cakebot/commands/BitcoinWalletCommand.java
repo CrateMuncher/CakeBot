96c7435a5bca79af2573f212ea6b45f16da6c2dc
Redodo <dodo@gododo.co>
2014-01-14 20:33:00 +0100

    A begin of the Bitcoin Wallet command

---
+++ src/main/java/net/cratemuncher/cakebot/commands/BitcoinWalletCommand.java
@@ -0,0 +1,17 @@
+package net.cratemuncher.cakebot.commands;
+
+import java.util.List;
+import net.cratemuncher.cakebot.CBCommand;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+
+public class BitcoinWalletCommand extends CBCommand {
+    public BitcoinWalletCommand() {
+        super("bitcoin-balance", "Shows the balance of a particular Bitcoin wallet.", "[A-Za-z0-9]{34}");
+    }
+
+    @Override
+    public void handle(GenericMessageEvent evt, List<String> args) {
+        evt.respond("This is a Bitcoin wallet address.");
+    }
+    
+}
